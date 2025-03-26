294b7f01427c5031e5bd781d5d64961a39956bba
israeliuMD <israeliu@gmail.com>
2025-03-26 09:40:41 +0200

    Update vite-config.js

--- vite-config.js
+++ vite-config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ocular-pathology-quiz/', // Add this line with your repo name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
